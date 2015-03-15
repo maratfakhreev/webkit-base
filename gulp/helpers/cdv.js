3fe4be2d1c808993cef387c7c36e0291fe73a4e2
maratfakhreev <marat.fakhreev@gmail.com>
2015-03-15 13:53:00 +0300

    Custom debouncer for correct cordova build preparing

--- gulp/helpers/cdv.js
+++ gulp/helpers/cdv.js
@@ -1,14 +1,17 @@
 var path = require('path');
 var transform = require('vinyl-transform');
 var cdv = require('cordova-lib').cordova.raw;
+var debounce = require('./debouncer');
 var config = require('../config');
 var root = process.cwd();
 var buildDir = path.join(root, config.buildDir);
 
-module.exports = function() {
-  if (!config.needPreparing) return;
+function cordovaPrepare() {
+  if (!config.preparing) return;
   process.chdir(buildDir);
   cdv.prepare().then(function() {
     process.chdir(root);
   });
-}
+};
+
+module.exports = debounce(cordovaPrepare, 1500);
