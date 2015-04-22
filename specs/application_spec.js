5e94b8c0944a3a518f10be14d6843709aa46e145
maratfakhreev <marat.fakhreev@flatstack.com>
2015-04-22 09:20:56 +0300

    Javascript Specs

---
+++ specs/application_spec.js
@@ -0,0 +1,8 @@
+import 'marionette';
+import App from 'scripts/application';
+
+describe('Application', function() {
+  it ('should start successfully', function() {
+    expect(App).to.be["instanceof"](Marionette.Application);
+  });
+});
